e5c1e9f77f9ae3643db64454dca45184df9ca226
comin7083 <164302311+comin7083@users.noreply.github.com>
2024-04-03 13:10:26 +0400

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/backend',backend );
 
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
